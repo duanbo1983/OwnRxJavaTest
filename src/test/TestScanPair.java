506fe0ee66b2c39cf9cf8678480d09c873e3b8f1
Bo Duan <bod@uber.com>
2016-09-01 17:34:40 -0700

    Close pool.

--- src/test/TestScanPair.java
+++ src/test/TestScanPair.java
@@ -1,5 +1,6 @@
 package test;
 
+import com.sun.tools.javac.util.Pair;
 import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func2;
@@ -12,6 +13,15 @@ public class TestScanPair {
     public static void main(String[] strings) {
 
         Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5, 6, 7, 8);
+
+        observable
+                .compose(new PairTransformer<>(0))
+                .subscribe(new Action1<Pair<Integer, Integer>>() {
+                    @Override
+                    public void call(Pair<Integer, Integer> integerIntegerPair) {
+                        System.out.println(integerIntegerPair);
+                    }
+                });
 
         observable.scan(new TwoInt(), new Func2<TwoInt, Integer, TwoInt>() {
             @Override
@@ -32,6 +42,38 @@ public class TestScanPair {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+
+
+    public static <T> Observable<Pair<T, T>> pair(T initValue, Observable<T> observable) {
+        Pair<T, T> pair = new Pair<>(initValue, initValue);
+        return observable.scan(pair, new Func2<Pair<T, T>, T, Pair<T, T>>() {
+            @Override
+            public Pair<T, T> call(Pair<T, T> ttPair, T t) {
+                return new Pair<>(ttPair.snd, t);
+            }
+        });
+    }
+
+    public static class PairTransformer<T> implements Observable.Transformer<T, Pair<T, T>> {
+
+        private T initValue;
+
+        PairTransformer(T initValue) {
+            this.initValue = initValue;
+        }
+
+        @Override
+        public Observable<Pair<T, T>> call(Observable<T> observable) {
+            Pair<T, T> pair = new Pair<>(initValue, initValue);
+            return observable.scan(pair, new Func2<Pair<T, T>, T, Pair<T, T>>() {
+                @Override
+                public Pair<T, T> call(Pair<T, T> ttPair, T t) {
+                    return new Pair<>(ttPair.snd, t);
+                }
+            });
         }
     }
 
